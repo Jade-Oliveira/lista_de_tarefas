d555db382377bceb9fb6ea170a209c2c32776824
Jade Oliveira - JOI <jade.oliveira@iteris.com.br>
2021-08-12 15:08:35 -0300

    App lista de tarefas finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ import 'package:path_provider/path_provider.dart';
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(primary: Colors.white),
-        ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(primary: Colors.white),
+      ),
     ),
     home: Home(),
   ));
@@ -21,7 +21,26 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _toDoController = TextEditingController();
+
   List _toDoList = [];
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+    //como o readData retorna um Future precisamos desse then para chamar a função dele assim que o readData retornar os dados
+    //readData retorna uma String que vai ser passado dentro dessa função anônima pelo parâmetro
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data!);
+      });
+    });
+  }
+
+  //vai ler os dados quando o meu app abre, aqui são salvos os dados permanetemente
+  //método que é chamado toda vez que inicializamos o estado da nosa tela
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +59,7 @@ class _HomeState extends State<Home> {
                 //expandimos o TextField para ter a maior largura possível
                 Expanded(
                   child: TextField(
+                    controller: _toDoController,
                     decoration: InputDecoration(
                         labelText: 'Nova Tarefa',
                         labelStyle: TextStyle(color: Colors.blueAccent)),
@@ -47,17 +67,93 @@ class _HomeState extends State<Home> {
                 ),
                 ElevatedButton(
                   style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent)
-                  ),
-                  onPressed: () {},
-                  child: Text('ADD', style: TextStyle(color: Colors.white),
+                      backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blueAccent)),
+                  onPressed: _addToDo,
+                  child: Text(
+                    'ADD',
+                    style: TextStyle(color: Colors.white),
                   ),
                 )
               ],
             ),
+          ),
+          //uso o expanded para informar que a lista deve ocupar a tela inteira
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              //builder é um construtor que vai permitir a construção da lista conforme eu for rodando ela
+              //ou seja, elementos escondidos não serão renderizados, e portanto não vão consumir recursos
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
+            ),
           )
         ],
       ),
+    );
+  }
+
+  Widget buildItem(context, index) {
+    //iwdget que vai permitir arrastar para a direita para excluir a tarefa
+    return Dismissible(
+      //pega o tempo em milisegundos e transforma em string, essa key aceita qualquer string, mas vai ter que ser diferente para todos os itens
+      key: Key(DateTime
+          .now()
+          .microsecondsSinceEpoch
+          .toString()),
+      background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(Icons.delete, color: Colors.white),
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]['title']),
+        value: _toDoList[index]['ok'],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]['ok'] ? Icons.check : Icons.error),
+        ),
+        //onChanged é chamado quando clico no elemento da lista, passando o parâmetro c como true ou false
+        onChanged: (c) {
+          setState(() {
+            //armazeno esse true ou false no ok do elemento da lista e dá um setState para atualizar a lista com o novo estado
+            _toDoList[index]['ok'] = c;
+            _saveData();
+          });
+        },
+      ),
+      //dentro desse onDismissed terá uma função que será chamada sempre que arrastar o item para a direita para remoção
+      onDismissed: (direction) {
+        setState(() {
+          //duplica o item
+          _lastRemoved = Map.from(_toDoList[index]);
+          //salvar o item
+          _lastRemovedPos = index;
+          //removemos o item
+          _toDoList.removeAt(index);
+          //salva a lista com item já removido
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text('Tarefa \'${_lastRemoved['title']}\' removida!'),
+            action: SnackBarAction(
+                label: 'Desfazer',
+                onPressed: () {
+                  setState(() {
+                    //recoloca o item removido na lista
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+          ScaffoldMessenger.of(context).removeCurrentSnackBar();
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
@@ -91,5 +187,37 @@ class _HomeState extends State<Home> {
     } catch (e) {
       return null;
     }
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      //pego o texto do meu textField
+      newToDo['title'] = _toDoController.text;
+      //zera o texto do textFiedl assim que clicar no botão para adicionar a tarefa
+      _toDoController.text = '';
+      //como acabamos de criar a tarefa, inicializa com ela não concluída
+      newToDo['ok'] = false;
+      //adicionamos um mapa na lista
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  //função que quando puxamos a tela para baixo ela carrega mais dados
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      //ordena a lista
+      _toDoList.sort((a, b){
+        if(a['ok'] && !b['ok']) return 1;
+        else if (!a['ok'] && b['ok']) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+
+    return null;
   }
 }
